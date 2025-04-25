7ad24fb1d8b6be208369a20e9c7c2fda41296792
ali nadri <alinadri@alis-Laptop.local>
2025-04-25 15:01:15 +0330

    its my first commit

---
+++ actions/signup.php
@@ -0,0 +1,36 @@
+<?php 
+require_once('../config/database.php');
+
+if (isset($_POST['signup'])){
+  try {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    //query
+    $query = "INSERT INTO users SET username=?, email=?, mobile=?, password=?";
+
+    //stmt
+    //prepare
+    $stmt = $conn->prepare($query);
+    //binde
+    $stmt->bindValue(1, $username);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $mobile);
+    $stmt->bindValue(4, $password);
+
+    //excute
+    $stmt->execute();
+    
+    echo "created account!";
+    header("Location: /loginsystem/index.php");
+    exit;
+  } catch(PDOException $e){
+    echo 'your eror is'.$e->getMessage();
+  }
+
+
+
+
+}
